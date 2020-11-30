0c8a39b15505d24476fa938f769fe75fc88589e3
karnsw <46457811+karnsw@users.noreply.github.com>
2020-11-29 19:20:14 -0500

    Add files via upload

---
+++ SandClock/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace SandClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 90);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "30min";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(464, 113);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(357, 308);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 534);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SandClock/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SandClock
+{
+    public partial class Form1 : Form
+    {
+        private int ticks = 0;
+        private bool clicked = false;
+        private int xpos = 0;
+        private int ypos = 0;
+        List<Bitmap> sand = new List<Bitmap>();
+        Bitmap finalImage = new Bitmap(100, 100);
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "ayyyyyy";
+            timer1.Start();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+  
+            //DrawFilledRectangle(500, 500);
+
+
+            var b = new Bitmap(1, 1);
+            b.SetPixel(0, 0, Color.Red);
+
+            var result = new Bitmap(b, 400, 400);
+            pictureBox1.Image = result;
+            pictureBox1.Image = DrawFilledRectangle(50,100);
+            
+
+
+            clicked = true;
+
+        }
+
+        private Bitmap DrawFilledRectangle(int x, int y)
+        {
+            Bitmap bmp = new Bitmap(x, y);
+            using (Graphics graph = Graphics.FromImage(bmp))
+            {
+                Rectangle ImageSize = new Rectangle(0, 0, x, y);
+                graph.FillRectangle(Brushes.Aqua, ImageSize);
+            }
+            return bmp;
+        }
+
+        private Bitmap AddFilledRectangle(int x, int y)
+        {
+            Bitmap bmp = new Bitmap(2, 2);
+            using (Graphics graph = Graphics.FromImage(bmp))
+            {
+                Rectangle ImageSize = new Rectangle(x, y, 2 , 2);
+                graph.FillRectangle(Brushes.Black, ImageSize);
+            }
+            return bmp;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //MessageBox.Show(DateTime.Now.ToString());
+            ticks++;
+
+
+
+
+
+            if (clicked == true)
+            {
+                Border();
+
+                
+                this.Text = ticks.ToString();
+                /*
+                sand.Add(AddFilledRectangle(xpos, ypos));
+                this.label1.Text = "xpos- " + xpos;
+                this.label2.Text = "size- " + sand.Count;
+
+                using (Graphics g = Graphics.FromImage(finalImage))
+                {
+                    g.Clear(Color.White);
+                    int offsetX = 0;
+                    int offsetY = 0;
+                    foreach (Bitmap pixel in sand)
+                    {
+                        Bitmap bmp = new Bitmap(2, 2);
+                        using (Graphics graph = Graphics.FromImage(bmp))
+                        {
+                            Rectangle ImageSize = new Rectangle(0, 0, 2, 2);
+                            graph.FillRectangle(Brushes.Aqua, ImageSize);
+                        }
+                        g.DrawImage(bmp, new Rectangle(offsetX, offsetY, pixel.Width, pixel.Height));
+                        offsetX = (offsetX + 2);
+                        if(offsetX >= finalImage.Width)
+                        {
+                            offsetX = 0;
+                            offsetY = offsetY + 2;
+                        }
+                    }
+
+
+                    xpos = xpos + 2;
+                    ypos = ypos + 2;
+                    this.label1.Text = "xpos- " + xpos;
+                    this.label2.Text = "size- " + sand.Count;
+                    pictureBox1.Image = finalImage;
+
+
+                    //finalImage.Dispose();
+                }
+                //this.pictureBox1.Image = finalImage;
+                //sand.Clear();
+
+                */
+
+                if (ticks == 10)
+                {
+                    this.Text = "Cheese";
+                    //timer1.Stop();
+                }
+                if(ticks >  13)
+                {
+                    timer1.Stop();
+                    timer1.Start();
+                    ticks = 0;
+                    Console.WriteLine(this.Text = ticks.ToString());
+                }
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Border()
+        {
+            int xTL = finalImage.Width / 4;
+            int yTL = finalImage.Height / 4;
+
+            int xTR = (3*finalImage.Width) / 4;
+            int yTR = finalImage.Height / 4;
+
+            int xBL = finalImage.Width / 4;
+            int yBL = (3*finalImage.Height) / 4;
+
+            int xBR = (3 * finalImage.Width) / 4;
+            int yBR = (3 * finalImage.Height) / 4;
+
+            int lineWidth = finalImage.Width / 8;
+            int lineHeight = finalImage.Height / 8;
+
+
+
+            using (Graphics g = Graphics.FromImage(finalImage))
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                using (Pen graphPen = new Pen(Color.Blue, 0))
+                {
+                    g.DrawLine()
+                }
+
+
+                    g.Clear(Color.White);
+                //int offsetX = 0;
+                //int offsetY = 0;
+
+
+                for (int xPos = -5; xPos < 5 + 1; xPos++)
+                {
+                    int yPosTL = ((int)(.1 * Math.Pow(xPos, 3)));
+                    int yPosNEG = ((int)(-.1 * Math.Pow(xPos, 3)));
+                    int xPosTL = xPos;
+                    Console.WriteLine( "x- " + xPosTL.ToString());
+                    Console.WriteLine( "y- " + yPosTL.ToString());
+
+
+                    
+                        this.label2.Text = "PRINT";
+                        Bitmap bmp = new Bitmap(2, 2);
+                        using (Graphics graph = Graphics.FromImage(bmp))
+                        {
+                            Rectangle ImageSize = new Rectangle(0, 0, 2, 2);
+                            graph.FillRectangle(Brushes.Black, ImageSize);
+                        }
+                        g.DrawImage(bmp, new Rectangle(xPosTL + xTL, yPosTL + yTL, bmp.Width, bmp.Height));
+                        g.DrawImage(bmp, new Rectangle(xPosTL + xTR, yPosNEG + yTR, bmp.Width, bmp.Height));
+                        g.DrawImage(bmp, new Rectangle(xPosTL + xBL, yPosTL + yBL, bmp.Width, bmp.Height));
+                        g.DrawImage(bmp, new Rectangle(xPosTL + xBR, yPosNEG + yBR, bmp.Width, bmp.Height));
+                }
+            }
+            pictureBox1.Image = finalImage;
+        }
+    }  
+}
